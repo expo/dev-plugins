fce964c03de38260ab8f9836794022168a302379
Kudo Chien <kudo@expo.dev>
2023-12-21 01:58:31 +0800

    [packages] inline process.env.NODE_ENV types

--- packages/react-navigation/src/index.ts
+++ packages/react-navigation/src/index.ts
@@ -1,3 +1,11 @@
+declare global {
+  const process: {
+    env: {
+      NODE_ENV: 'development' | 'production';
+    };
+  };
+}
+
 export let useReactNavigationDevTools: typeof import('./useReactNavigationDevTools').useReactNavigationDevTools;
 
 if (process.env.NODE_ENV !== 'production') {
