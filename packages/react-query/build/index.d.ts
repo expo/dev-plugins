ae8d73573b3db6cd47ee126682e5ea4ab002a419
Kudo Chien <kudo@expo.dev>
2023-12-25 17:15:23 +0800

    update to expo sdk 50 beta

--- packages/react-query/build/index.d.ts
+++ packages/react-query/build/index.d.ts
@@ -1,2 +1,9 @@
+declare global {
+    const process: {
+        env: {
+            NODE_ENV: 'development' | 'production';
+        };
+    };
+}
 export declare let useReactQueryDevTools: typeof import('./useReactQueryDevTools').useReactQueryDevTools;
 //# sourceMappingURL=index.d.ts.map
