5a58dfc4a5b9dad8e81e4302c7bbe54c06206563
Kudo Chien <kudo@expo.dev>
2023-12-29 16:26:48 +0800

    [packages] remove process declarations (#17)
    # Why
    
    fixes #16 
    
    # How
    
    rather than declare inline types, just ignore the errors since we just
    use `process.env.NODE_ENV` once.

--- packages/apollo-client/src/index.ts
+++ packages/apollo-client/src/index.ts
@@ -1,13 +1,6 @@
-declare global {
-  const process: {
-    env: {
-      NODE_ENV: 'development' | 'production';
-    };
-  };
-}
-
 export let useApolloClientDevTools: typeof import('./useApolloClientDevTools').useApolloClientDevTools;
 
+// @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== 'production') {
   useApolloClientDevTools = require('./useApolloClientDevTools').useApolloClientDevTools;
 } else {
--- packages/react-query/src/index.ts
+++ packages/react-query/src/index.ts
@@ -1,13 +1,6 @@
-declare global {
-  const process: {
-    env: {
-      NODE_ENV: 'development' | 'production';
-    };
-  };
-}
-
 export let useReactQueryDevTools: typeof import('./useReactQueryDevTools').useReactQueryDevTools;
 
+// @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== 'production') {
   useReactQueryDevTools = require('./useReactQueryDevTools').useReactQueryDevTools;
 } else {
