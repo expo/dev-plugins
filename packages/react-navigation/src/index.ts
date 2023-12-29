e7f5f6baa50fb65805036ee8a04f88b5db828e2c
Kudo Chien <kudo@expo.dev>
2023-12-29 16:24:06 +0800

    [packages] remove process declarations

--- packages/react-navigation/src/index.ts
+++ packages/react-navigation/src/index.ts
@@ -1,13 +1,6 @@
-declare global {
-  const process: {
-    env: {
-      NODE_ENV: 'development' | 'production';
-    };
-  };
-}
-
 export let useReactNavigationDevTools: typeof import('./useReactNavigationDevTools').useReactNavigationDevTools;
 
+// @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== 'production') {
   useReactNavigationDevTools = require('./useReactNavigationDevTools').useReactNavigationDevTools;
 } else {
--- packages/tinybase/src/index.ts
+++ packages/tinybase/src/index.ts
@@ -1,13 +1,6 @@
-declare global {
-  const process: {
-    env: {
-      NODE_ENV: 'development' | 'production';
-    };
-  };
-}
-
 export let useTinyBaseDevTools: typeof import('./useTinyBaseDevTools').useTinyBaseDevTools;
 
+// @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== 'production') {
   useTinyBaseDevTools = require('./useTinyBaseDevTools').useTinyBaseDevTools;
 } else {
