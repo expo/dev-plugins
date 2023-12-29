6dc100962aa09c6e01a27628fe97e9bef836d2d2
Kudo Chien <kudo@expo.dev>
2023-12-29 16:32:40 +0800

    remove process declarations

--- packages/create-dev-plugin/templates/app-adapter/src/index.ts
+++ packages/create-dev-plugin/templates/app-adapter/src/index.ts
@@ -1,13 +1,6 @@
-declare global {
-  const process: {
-    env: {
-      NODE_ENV: 'development' | 'production';
-    };
-  };
-}
-
 export let <%= project.hookName %>: typeof import('./<%= project.hookName %>').<%= project.hookName %>;
 
+// @ts-ignore process.env.NODE_ENV is defined by metro transform plugins
 if (process.env.NODE_ENV !== 'production') {
   <%= project.hookName %> = require('./<%= project.hookName %>').<%= project.hookName %>;
 } else {
