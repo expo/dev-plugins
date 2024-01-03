7fa795fb2a4c1dc25280758da3768fce32b296e9
Joshua Tag Howard <tag@jthoward.dev>
2024-01-02 19:27:40 -0600

    Add checkboxes to create JSON values instead of text

--- packages/async-storage/webui/src/modal/useAddEntryDialog.tsx
+++ packages/async-storage/webui/src/modal/useAddEntryDialog.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Modal } from 'antd';
+import { Checkbox, Form, Input, Modal } from 'antd';
 import { ReactElement, useCallback, useState } from 'react';
 
 export function useAddEntryDialog({
@@ -14,11 +14,15 @@ export function useAddEntryDialog({
 
   const [key, setKey] = useState('');
   const [value, setValue] = useState('');
+  const [emptyJson, setEmptyJson] = useState(false);
+  const [emptyArray, setEmptyArray] = useState(false);
 
   const reset = useCallback(() => {
     setKey('');
     setValue('');
-  }, [setKey, setValue]);
+    setEmptyJson(false);
+    setEmptyArray(false);
+  }, [setKey, setValue, setEmptyJson, setEmptyArray]);
 
   const showAddEntryDialog = useCallback(() => {
     reset();
@@ -39,19 +43,49 @@ export function useAddEntryDialog({
             reset();
           }}
           onOk={() => {
-            set(key, value).then(() => {
+            let valueToSet = value;
+            if (emptyJson && !emptyArray) {
+              valueToSet = '{}';
+            } else if (emptyArray && !emptyJson) {
+              valueToSet = '[]';
+            } else if (emptyArray && emptyJson) {
+              alert('Cannot create empty JSON object and array at the same time.');
+              return;
+            }
+            set(key, valueToSet).then(() => {
               setShown(false);
               reset();
             });
           }}
           okButtonProps={{
-            disabled: !key || !value,
+            disabled: !key || (!value && !emptyJson && !emptyArray),
           }}>
           <Form.Item label="Key">
             <Input value={key} onChange={(e) => setKey(e.target.value)} required />
           </Form.Item>
           <Form.Item label="Value">
-            <Input.TextArea value={value} onChange={(e) => setValue(e.target.value)} required />
+            <Input.TextArea
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              required
+              disabled={emptyJson || emptyArray}
+            />
+          </Form.Item>
+          <Form.Item label="JSON">
+            <Checkbox
+              checked={emptyJson && !emptyArray}
+              onChange={(e) => setEmptyJson(e.target.checked)}
+              disabled={emptyArray}>
+              Create empty JSON object
+            </Checkbox>
+          </Form.Item>
+          <Form.Item label="Array">
+            <Checkbox
+              checked={emptyArray && !emptyJson}
+              onChange={(e) => setEmptyArray(e.target.checked)}
+              disabled={emptyJson}>
+              Create empty JSON array
+            </Checkbox>
           </Form.Item>
         </Modal>
       </Form>
