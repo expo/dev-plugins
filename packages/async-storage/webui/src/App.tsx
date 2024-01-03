19bd05bbe28786198cc85ce86f68a7ec60d3685b
Joshua Tag Howard <tag@jthoward.dev>
2024-01-02 20:30:46 -0600

    Fix typo

--- packages/async-storage/webui/src/App.tsx
+++ packages/async-storage/webui/src/App.tsx
@@ -6,7 +6,7 @@ export default function Main() {
     <App style={{ width: '100%', height: '100%', padding: '0.75em' }}>
       <AsyncStorageTable />
       <p>
-        By default, the reload button will not update any fields you have changes. To fully update
+        By default, the reload button will not update any fields you have changed. To fully update
         the list hold the <code>Shift</code> key when clicking the reload button.
       </p>
     </App>
