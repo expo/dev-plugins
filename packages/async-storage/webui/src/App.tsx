a34bc5e5578e8f2702e835fa728327591e96dbbb
Marius Jørgensen <5442549+marjorg@users.noreply.github.com>
2024-02-07 14:10:24 +0100

    fix(async-storage): Scrolling on the Y axis (#26)
    Was playing around with the plugin in one of my projects, noticed that
    it was not possible to scroll when the list was long.

--- packages/async-storage/webui/src/App.tsx
+++ packages/async-storage/webui/src/App.tsx
@@ -3,7 +3,7 @@ import { AsyncStorageTable } from './AsyncStorageTable';
 
 export default function Main() {
   return (
-    <App style={{ width: '100%', height: '100%', padding: '0.75em' }}>
+    <App style={{ width: '100%', height: '100%', padding: '0.75em', overflowY: 'scroll' }}>
       <AsyncStorageTable />
       <p>
         By default, the reload button will not update any fields you have changed. To fully update
