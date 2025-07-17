5ecf8111e480d9b0ec69ffa090822598ee01d9cb
Kudo Chien <kudo@expo.dev>
2025-07-17 23:29:59 +0800

    [create-dev-plugin] fix EXPO_BETA enabled by default (#74)
    # Why
    
    fix that we use incorrect template to create the webui project for
    create-dev-plugin
    
    # How
    
    turn `EXPO_BETA` to disabled by default

--- packages/create-dev-plugin/src/env.ts
+++ packages/create-dev-plugin/src/env.ts
@@ -1,4 +1,3 @@
 import getenv from 'getenv';
 
-// FIXME: disable default BETA when SDK 50 is officially released.
-export const EXPO_BETA = getenv.boolish('EXPO_BETA', true);
+export const EXPO_BETA = getenv.boolish('EXPO_BETA', false);
